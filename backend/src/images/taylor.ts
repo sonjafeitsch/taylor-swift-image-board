f01d835f23763ac2a37c6c27f74242966aa3396b
Sonja Feitsch <feitsch@e2n.de>
2024-11-07 19:29:00 +0100

    update masonry

--- backend/src/images/taylor.ts
+++ backend/src/images/taylor.ts
@@ -4,12 +4,12 @@ export const taylorSwiftImages: Image[] = [
   {
     id: 0,
     description: 'Taylor Swift performing on stage during the Eras Tour',
-    url: 'https://www.teenvogue.com/story/taylor-swift-eras-tour-outfits',
+    url: 'https://assets.teenvogue.com/photos/641b2a23912ddccbabf80f80/16:9/w_2240,c_limit/GettyImages-1474459622.jpg',
   },
   {
     id: 1,
     description: 'Taylor Swift in a red dress at an award show',
-    url: 'https://www.rollingstone.com/music/music-news/taylor-swift-les-miserables-nightmare-audition-1234620509/',
+    url: 'https://www.rollingstone.com/wp-content/uploads/2022/10/taylor-swift-les-miserables.jpg?w=1581&h=1054&crop=1',
   },
   {
     id: 2,
@@ -19,11 +19,36 @@ export const taylorSwiftImages: Image[] = [
   {
     id: 3,
     description: 'Taylor Swift posing for a magazine photoshoot',
-    url: 'https://api.time.com/wp-content/uploads/2023/12/taylor-swift-person-of-the-year-cat.jpg',
+    url: 'https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/KUQN5M3L6FSH5HQLEOX4UO7DSU.JPG&w=1200',
   },
   {
     id: 4,
     description: 'Taylor Swift performing at a music festival',
     url: 'https://people.com/thmb/hBHRstgBtJf8fHk7nCpa_46rO-o=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(700x238:702x240)/taylor-swift-072324-a0b5ec36a78a41469f0eef49a2e28989.jpg',
+  },
+  {
+    id: 5,
+    description: 'Tortured Poets Cover',
+    url: 'https://upload.wikimedia.org/wikipedia/en/6/6e/Taylor_Swift_%E2%80%93_The_Tortured_Poets_Department_%28album_cover%29.png',
+  },
+  {
+    id: 6,
+    description: 'Suprise Songs',
+    url: 'https://www.nj.com/resizer/v2/RFZ5CZL37NBHBPGWDJSB6DUUTM.jpg?auth=8461c4fec52b47bf2a3cf66210a86af268ae6d613b8bfe64f4adf07fe798cf24&width=500&quality=90',
+  },
+  {
+    id: 7,
+    description: 'Evermore',
+    url: 'https://www.jamesfrew.me/content/images/2024/01/Eo4M55KXEAI6OGV.jpg',
+  },
+  {
+    id: 8,
+    description: 'Grammys',
+    url: 'https://i8.amplience.net/i/naras/Taylor-Swift-AOTY-2024-GRAMMYs',
+  },
+  {
+    id: 9,
+    description: 'VMAs',
+    url: 'https://assets.vogue.com/photos/66e2300d48fba0d46386509f/master/w_2560%2Cc_limit/2171410987',
   },
 ];
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,9 +6,14 @@ function App() {
   console.log(images);
 
   return (
-    <>
+    <div className="flex flex-col justify-start gap-4 w-full">
       <h1>Hello Taylor</h1>
-    </>
+      <div className="masonry">
+        {images.map((image) => (
+          <img src={image.url} key={image.id} style={{ width: "100%" }} />
+        ))}
+      </div>
+    </div>
   );
 }
 
